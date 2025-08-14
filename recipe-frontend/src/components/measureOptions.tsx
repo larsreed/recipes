875041d83715ab902691fffdefb380260cce2af7
Lars Reed <lars.reed@toll.no>
2025-08-14 22:10:32 +0200

    more measures

--- recipe-frontend/src/components/measureOptions.tsx
+++ recipe-frontend/src/components/measureOptions.tsx
@@ -1,5 +1,6 @@
 const measureOptions = [
     '', 'ts', 'tsp', 'tbsp', 'ss', 'ml', 'cl', 'dl', 'l', 'mg', 'g', 'kg', 'stk', 'pcs',
-    'kopper', 'cups', 'leaves', 'blader', 'baskets', 'kurver', 'pinches', 'klyper', 'pots', 'potter'
+    'kopper', 'cups', 'leaves', 'blader', 'baskets', 'kurver', 'pinches', 'klyper', 'pots', 'potter',
+    'fedd', 'cloves', 'knivsodder', 'glass', 'glasses', 'flasker', 'bottles', 'jars', 'pakker', 'packages'
 ];
 export default measureOptions;
