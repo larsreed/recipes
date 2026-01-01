e2e8af25053499802e1724d81a9b6490180c73dd
Lars Reed <lars.reed@toll.no>
2026-01-01 21:14:17 +0100

    bs

--- recipe-frontend/src/components/measureOptions.tsx
+++ recipe-frontend/src/components/measureOptions.tsx
@@ -1,5 +1,5 @@
 const measureOptions = [
-    '', 'ts', 'tsp', 'tbsp', 'ss', 'ml', 'cl', 'dl', 'l', 'mg', 'g', 'kg', 'stk', 'pcs',
+    '', 'bs', 'ts', 'tsp', 'tbsp', 'ss', 'ml', 'cl', 'dl', 'l', 'mg', 'g', 'kg', 'stk', 'pcs',
     'kopper', 'cups', 'leaves', 'blader', 'baskets', 'kurver', 'pinches', 'klyper', 'pots', 'potter',
     'fedd', 'cloves', 'knivsodder', 'glass', 'glasses', 'flasker', 'bottles', 'jars', 'pakker', 'packages',
     'dråper', 'drops', 'skiver', 'slices', 'biter', 'pieces', 'håndfuller', 'handfuls'
